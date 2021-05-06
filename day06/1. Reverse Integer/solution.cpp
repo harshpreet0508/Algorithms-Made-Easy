20220d35a2707b38499fcfc676fa1691c7261ff6
Harshpreet Kaur <harshpreet0508@gmail.com>
2021-05-06 23:28:29 +0530

    Create solution.cpp

---
+++ day06/1. Reverse Integer/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int reverse(int x) 
+    {
+        int r = 0;
+        while(x != 0)
+        {
+
+            if ( r > INT_MAX / 10 || (r == INT_MAX / 10 && x % 10 > 7) )
+                return 0;
+           
+            if ( r < INT_MIN / 10 || (r == INT_MIN / 10 && x % 10 < -8) ) 
+                return 0;
+            
+            r = r * 10 + x % 10;
+            x /= 10;
+        }
+        
+        return r;
+    }
+};
